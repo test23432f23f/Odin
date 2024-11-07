87340731144007bf4d0668c711f1c76a1ff25164
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-08 00:01:32 +0100

    Update ModCore.kt

--- odinclient/src/main/kotlin/me/odinclient/ModCore.kt
+++ odinclient/src/main/kotlin/me/odinclient/ModCore.kt
@@ -22,7 +22,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import me.odinclient.commands.impl.RouteCommand
 import me.odinclient.utils.skyblock.AutoRouteUtils
-import net.minecraftforge.common.MinecraftForge
 
 @Suppress("UNUSED_PARAMETER")
 @Mod(
@@ -64,7 +63,7 @@ class ModCore {
             FarmingHitboxes, NoBlock, AutoClicker, Triggerbot, GhostBlocks, FreezeGame, EtherWarpHelper, ChestEsp,
             EscrowFix, TerminalAura, AutoTerms, Camera, DungeonAbilities, QueueTerms, HidePlayers
         )
-        MinecraftForge.EVENT_BUS.register(AutoRouteUtils)
+        MinecraftForge.EVENT_BUS.register(new AutoRouteUtils())
         OdinMain.loadComplete()
     }
 
