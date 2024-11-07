57db1b3097665a49aef67dbbdeab96973316c61a
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-07 22:18:37 +0100

    Add files via upload

---
+++ odinclient/src/main/java/me/odinclient/mixin/accessors/IEntityPlayerSPAccessor.java
@@ -0,0 +1,27 @@
+package org.cata.mixin.entity;
+
+import net.minecraft.client.entity.EntityPlayerSP;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityPlayerSP.class)
+public interface IEntityPlayerSPAccessor
+{
+    @Accessor("lastReportedPosX")
+    double getLastReportedPosX();
+
+    @Accessor("lastReportedPosY")
+    double getLastReportedPosY();
+
+    @Accessor("lastReportedPosZ")
+    double getLastReportedPosZ();
+
+    @Accessor("lastReportedYaw")
+    float getLastReportedYaw();
+
+    @Accessor("lastReportedPitch")
+    float getLastReportedPitch();
+
+    @Accessor("positionUpdateTicks")
+    int getPositionUpdateTicks();
+}
