82eab87a1cdb6735496b01df8072729540240ffe
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-08 01:25:16 +0100

    Update AutoRouteUtils.java

--- odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.java
+++ odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.java
@@ -1,143 +1,114 @@
-package me.odinclient.utils.skyblock;
+package me.odinclient.utils.skyblock
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.play.client.C03PacketPlayer;
-import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
-import net.minecraft.util.BlockPos;
-import net.minecraft.util.Vec3;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import me.odinmain.events.impl.PacketReceivedEvent;
-import me.odinmain.events.impl.PacketSentEvent;
-import me.odinclient.utils.skyblock.RoutesManager;
-import me.odinclient.utils.skyblock.Timer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraft.client.Minecraft;
-import me.odinmain.features.Module;
-import net.minecraft.client.entity.EntityPlayerSP;
-import me.odinmain.features.Category;
-import me.odinclient.mixin.accessors.IEntityPlayerSPAccessor;
-import me.odinmain.events.impl.DungeonEvents.RoomEnterEvent;
-import me.odinmain.utils.skyblock.dungeon.tiles.Room;
+import me.odinmain.events.impl.DungeonEvents.RoomEnterEvent
+import me.odinmain.events.impl.PacketSentEvent
+import net.minecraft.client.Minecraft
+import net.minecraft.item.ItemStack
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.client.C03PacketPlayer.C05PacketPlayerLook
+import net.minecraft.network.play.client.C03PacketPlayer.C06PacketPlayerPosLook
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement
+import net.minecraft.util.BlockPos
+import net.minecraft.util.Vec3
+import me.odinmain.utils.skyblock.dungeon.DungeonUtils
+import me.odinmain.utils.skyblock.dungeon.DungeonUtils.getRealCoords
+import me.odinmain.utils.skyblock.dungeon.DungeonUtils.getRelativeCoords
+import net.minecraftforge.client.event.RenderWorldLastEvent
+import me.odinmain.utils.skyblock.dungeon.tiles.Room
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
+import org.cata.manager.RoutesManager
+import org.cata.mixin.entity.IEntityPlayerSPAccessor
+import org.cata.util.minecraft.RenderUtils
+import java.awt.Color
+import java.util.*
+import java.util.stream.Collectors
 
-import java.awt.*;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import me.odinmain.utils.skyblock.dungeon.DungeonUtils;
-
-public class AutoRouteUtils
-{
-    public static Room currentRoom = null;
-    public static String currentRoomName = "Unknown";
+class AutoRouteUtils {
     @SubscribeEvent
-    public void onRoom(RoomEnterEvent event)
-    {
-        currentRoom = event.getRoom();
-        String name = event.getRoom().getData() != null ? event.getRoom().getData().getName() : "Unknown";
-        currentRoomName = name;
+    fun onRoom(event: RoomEnterEvent) {
+        currentRoom = event.getRoom()
+        val name = if (event.getRoom().getData() != null) event.getRoom().getData().getName() else "Unknown"
+        currentRoomName = name
     }
-    
-    protected final Minecraft mc = Minecraft.getMinecraft();
-    public Color color = new Color(0xFF10FD);
-    public double tolerance = 0.7;
 
-    public static int rotationDelay = 125, etherDelay = 150;
-    public boolean rotationQueued, etherQueued;
-
+    protected val mc = Minecraft.getMinecraft()
+    var color = Color(0xFF10FD)
+    var tolerance = 0.7
+    var rotationQueued = false
+    var etherQueued = false
     @SubscribeEvent
-    public void onRender(RenderWorldLastEvent event)
-    {
-        if(RoutesManager.instance.loadedRoutes.isEmpty() || RoutesManager.instance.loadedRoutes.get(currentRoom) == null)
-        {
-            return;
+    fun onRender(event: RenderWorldLastEvent?) {
+        if (RoutesManager.instance.loadedRoutes.isEmpty() || RoutesManager.instance.loadedRoutes.get(currentRoom) == null) {
+            return
         }
-
-        RoutesManager.Route lastRoute;
-        for(int id : RoutesManager.instance.loadedRoutes.get(currentRoom).keySet())
-        {
-            lastRoute = null;
-            for(RoutesManager.Route route : RoutesManager.instance.loadedRoutes.get(currentRoom).get(id))
-            {
-                if(lastRoute != null)
-                    RenderUtils.drawLine(
-                            currentRoom.getRealCoords(lastRoute.pos), currentRoom.getRealCoords(route.pos),
-                            color.brighter());
-
-                    RenderUtils.blockBox(new BlockPos(currentRoom.getRealCoords(route.pos)), route.subId == 0 ? color.darker().darker() : color);
-
-                lastRoute = route;
+        var lastRoute: RoutesManager.Route?
+        for (id in RoutesManager.instance.loadedRoutes.get(currentRoom)!!.keys) {
+            lastRoute = null
+            for (route in RoutesManager.instance.loadedRoutes.get(currentRoom)!![id]!!) {
+                if (lastRoute != null) RenderUtils.drawLine(
+                    currentRoom.getRealCoords(lastRoute.pos), currentRoom.getRealCoords(route.pos),
+                    color.brighter()
+                )
+                RenderUtils.blockBox(
+                    BlockPos(currentRoom.getRealCoords(route.pos)),
+                    if (route.subId == 0) color.darker().darker() else color
+                )
+                lastRoute = route
             }
         }
     }
 
     @SubscribeEvent
-    public void onPacket(PacketSentEvent event)
-    {
-        if (!(event.getPacket() instanceof C03PacketPlayer) || !cancelling)
-        {
-            return;
+    fun onPacket(event: PacketSentEvent) {
+        if (event.getPacket() !is C03PacketPlayer || !cancelling) {
+            return
         }
-        if (!event.isCanceled())
-        {
-            event.setCanceled(true);
+        if (!event.isCanceled()) {
+            event.setCanceled(true)
         }
-        cancelling = false;
+        cancelling = false
     }
 
-
-    Timer rotationTimer = new Timer();
-    Timer etherTimer = new Timer();
-
+    var rotationTimer: Timer = Timer()
+    var etherTimer: Timer = Timer()
     @SubscribeEvent
-    public void onUpdate(TickEvent.ClientTickEvent event)
-    {
-        if(mc.thePlayer == null)
-        {
-            return;
+    fun onUpdate(event: ClientTickEvent?) {
+        if (mc.thePlayer == null) {
+            return
         }
-        if(RoutesManager.instance.loadedRoutes.isEmpty() || RoutesManager.instance.loadedRoutes.get(currentRoom) == null)
-        {
-            return;
+        if (RoutesManager.instance.loadedRoutes.isEmpty() || RoutesManager.instance.loadedRoutes.get(currentRoom) == null) {
+            return
         }
-        for(String roomId : RoutesManager.instance.loadedRoutes.keySet())
-        {
-            for(int id : RoutesManager.instance.loadedRoutes.get(roomId).keySet())
-            {
-                List<RoutesManager.Route> routes = RoutesManager.instance.loadedRoutes.get(roomId).get(id)
-                        .stream().sorted(Comparator.comparingInt(r -> r.subId)).collect(Collectors.toList());
-                for(int i = 0; i < routes.size(); i++)
-                {
-                    if(routes.size() < 2) continue;
-
-                    RoutesManager.Route route = routes.get(i);
-
-                    if(new Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ).distanceTo(currentRoom.getRealCoords(route.pos))
-                            <= tolerance && i < routes.size() && i + 1 < routes.size() && (getSkyBlockID(mc.thePlayer.getHeldItem())
-                            .equals("ASPECT_OF_THE_VOID") || getDisplayName(mc.thePlayer.getHeldItem()).toLowerCase().contains("aspect of the void")) && mc.thePlayer.isSneaking())
-                    {
-                        RoutesManager.Route nextRoute = routes.get(i + 1);
-
-                        float yaw;
-                        float pitch;
-
-                        yaw = route.yaw;
-                        pitch = route.pitch;
-
-                        if(rotationTimer.hasPassed(rotationDelay))
-                        {
-                            cancelRotate(yaw, pitch);
-                        
-                            rotationTimer.reset();
+        for (roomId in RoutesManager.instance.loadedRoutes.keys) {
+            for (id in RoutesManager.instance.loadedRoutes[roomId]!!.keys) {
+                val routes = RoutesManager.instance.loadedRoutes[roomId]!![id]
+                    .stream().sorted(Comparator.comparingInt { r: RoutesManager.Route -> r.subId })
+                    .collect(Collectors.toList())
+                for (i in routes.indices) {
+                    if (routes.size < 2) continue
+                    val route = routes[i]
+                    if ((Vec3(
+                            mc.thePlayer.posX,
+                            mc.thePlayer.posY,
+                            mc.thePlayer.posZ
+                        ).distanceTo(currentRoom.getRealCoords(route.pos))
+                                <= tolerance) && i < routes.size && i + 1 < routes.size && ((getSkyBlockID(mc.thePlayer.heldItem)
+                                == "ASPECT_OF_THE_VOID") || getDisplayName(mc.thePlayer.heldItem).lowercase(Locale.getDefault())
+                            .contains("aspect of the void")) && mc.thePlayer.isSneaking
+                    ) {
+                        val nextRoute = routes[i + 1]
+                        var yaw: Float
+                        var pitch: Float
+                        yaw = route.yaw
+                        pitch = route.pitch
+                        if (rotationTimer.hasPassed(rotationDelay)) {
+                            cancelRotate(yaw, pitch)
+                            rotationTimer.reset()
                         }
-
-                        if(etherTimer.hasPassed(etherDelay))
-                        {
-                            mc.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.getHeldItem()));
-                            etherTimer.reset();
+                        if (etherTimer.hasPassed(etherDelay)) {
+                            mc.thePlayer.sendQueue.addToSendQueue(C08PacketPlayerBlockPlacement(mc.thePlayer.heldItem))
+                            etherTimer.reset()
                         }
                     }
                 }
@@ -145,61 +116,60 @@ public class AutoRouteUtils
         }
     }
 
-    public boolean cancelling = false;
-
-    public void cancelRotate(float yaw, float pitch)
-    {
-        IEntityPlayerSPAccessor player = (IEntityPlayerSPAccessor) mc.thePlayer;
-        if (player == null)
-        {
-            return;
-        }
-        double x = mc.thePlayer.posX - player.getLastReportedPosX();
-        double y = mc.thePlayer.posY - player.getLastReportedPosX();
-        double z = mc.thePlayer.posZ - player.getLastReportedPosZ();
-        boolean moving = x * x + y * y + z * z > 9.0E-4 || player.getPositionUpdateTicks() >= 20;
+    var cancelling = false
+    fun cancelRotate(yaw: Float, pitch: Float) {
+        val player = mc.thePlayer as IEntityPlayerSPAccessor ?: return
+        val x = mc.thePlayer.posX - player.lastReportedPosX
+        val y = mc.thePlayer.posY - player.lastReportedPosX
+        val z = mc.thePlayer.posZ - player.lastReportedPosZ
+        val moving = x * x + y * y + z * z > 9.0E-4 || player.positionUpdateTicks >= 20
         if (moving) {
             //ChatLib.sendf("C06");
-            mc.getNetHandler().getNetworkManager().sendPacket(
-                    new C03PacketPlayer.C06PacketPlayerPosLook(
-                            mc.thePlayer.posX,
-                            mc.thePlayer.posY,
-                            mc.thePlayer.posZ,
-                            yaw,
-                            pitch,
-                            mc.thePlayer.onGround
-                    )
-            );
-        } else {
-            //ChatLib.sendf("C05");
-            mc.getNetHandler().getNetworkManager().sendPacket(new C03PacketPlayer.C05PacketPlayerLook(
+            mc.netHandler.networkManager.sendPacket(
+                C06PacketPlayerPosLook(
+                    mc.thePlayer.posX,
+                    mc.thePlayer.posY,
+                    mc.thePlayer.posZ,
                     yaw,
                     pitch,
                     mc.thePlayer.onGround
-            ));
+                )
+            )
+        } else {
+            //ChatLib.sendf("C05");
+            mc.netHandler.networkManager.sendPacket(
+                C05PacketPlayerLook(
+                    yaw,
+                    pitch,
+                    mc.thePlayer.onGround
+                )
+            )
         }
-        cancelling = true;
-    }
-    
-    public List<RoutesManager.Route> getSortedRoutes(String roomId, int routeId)
-    {
-        return RoutesManager.instance.loadedRoutes.get(roomId).get(routeId).stream().sorted(Comparator.comparingInt(r -> r.id)).collect(Collectors.toList());
+        cancelling = true
     }
 
-      public static String getDisplayName(ItemStack stack)
-    {
-        return stack != null ? stack.hasDisplayName() ? stack.getDisplayName() : "" : "";
+    fun getSortedRoutes(roomId: String?, routeId: Int): List<RoutesManager.Route> {
+        return RoutesManager.instance.loadedRoutes.get(roomId)!![routeId]!!.stream()
+            .sorted(Comparator.comparingInt { r: RoutesManager.Route -> r.id }).collect(Collectors.toList())
     }
 
-    public static String getSkyBlockID(ItemStack item)
-    {
-        if(item != null)
-        {
-            NBTTagCompound extraAttributes = item.getSubCompound("ExtraAttributes", false);
-            if(extraAttributes != null && extraAttributes.hasKey("id")) {
-                return extraAttributes.getString("id");
+    companion object {
+        var currentRoom: Room? = null
+        var currentRoomName = "Unknown"
+        var rotationDelay = 125
+        var etherDelay = 150
+        fun getDisplayName(stack: ItemStack?): String {
+            return if (stack != null) if (stack.hasDisplayName()) stack.displayName else "" else ""
+        }
+
+        fun getSkyBlockID(item: ItemStack?): String {
+            if (item != null) {
+                val extraAttributes = item.getSubCompound("ExtraAttributes", false)
+                if (extraAttributes != null && extraAttributes.hasKey("id")) {
+                    return extraAttributes.getString("id")
+                }
             }
+            return ""
         }
-        return "";
     }
 }
