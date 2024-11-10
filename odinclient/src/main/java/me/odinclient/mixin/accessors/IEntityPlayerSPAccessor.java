7409cdcfebd2d8a5d73229c1dec7d47636780a89
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-10 16:38:34 +0100

    Update IEntityPlayerSPAccessor.java

--- odinclient/src/main/java/me/odinclient/mixin/accessors/IEntityPlayerSPAccessor.java
+++ odinclient/src/main/java/me/odinclient/mixin/accessors/IEntityPlayerSPAccessor.java
@@ -25,5 +25,6 @@ public interface IEntityPlayerSPAccessor
     @Accessor("positionUpdateTicks")
     int getPositionUpdateTicks();
 
-     
+     @Accessor("serverSneakState")
+     boolean getServerSneakState();
 }
