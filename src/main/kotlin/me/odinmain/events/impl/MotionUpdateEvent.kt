f09b86f255130dda99b4882274658847f1093a18
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-10 10:10:18 +0100

    Update MotionUpdateEvent.kt

--- src/main/kotlin/me/odinmain/events/impl/MotionUpdateEvent.kt
+++ src/main/kotlin/me/odinmain/events/impl/MotionUpdateEvent.kt
@@ -1,1 +1,10 @@
+package me.odinmain.events.impl
 
+import net.minecraftforge.fml.common.eventhandler.Cancelable
+import net.minecraftforge.fml.common.eventhandler.Event
+
+/**
+ * Sent when a block has been updated.
+ */
+@Cancelable
+class BlockChangeEvent(var x: Double, var y: Double, var z: Double, var yaw: Float, var pitch: Float, var onGround: Boolean) : Event()
