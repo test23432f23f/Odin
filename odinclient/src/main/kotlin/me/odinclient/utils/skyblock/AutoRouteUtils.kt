cf27bd01ac454d8fb9cdd932bf16a6fbc10b76e9
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-10 11:11:13 +0100

    Update AutoRouteUtils.kt

--- odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
+++ odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
@@ -168,8 +168,7 @@ class AutoRouteUtils : Module(
                         event.yaw = yaw
                         event.pitch = pitch
 
-                        mc.thePlayer.renderYawOffset = preMotionUpdateEvent.getYaw();
-                        mc.thePlayer.rotationYawHead = preMotionUpdateEvent.getYaw();
+                        
 
                        
                        if(clickTimer.hasPassed(clickDelay))
