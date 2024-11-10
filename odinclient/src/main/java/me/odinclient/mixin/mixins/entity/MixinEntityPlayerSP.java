cb98770087b0b9343b8f5186ecc7ead5184ba0ef
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-10 10:27:53 +0100

    Update MixinEntityPlayerSP.java

--- odinclient/src/main/java/me/odinclient/mixin/mixins/entity/MixinEntityPlayerSP.java
+++ odinclient/src/main/java/me/odinclient/mixin/mixins/entity/MixinEntityPlayerSP.java
@@ -57,12 +57,10 @@ public abstract class MixinEntityPlayerSP {
 
         MotionUpdateEvent preMotionUpdateEvent = new MotionUpdateEvent(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ,
                 mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch, mc.thePlayer.onGround);
-        if(MinecraftForge.EVENT_BUS.post(preMotionUpdateEvent))
-        {
-             return
-        }
-
-
+        
+         if(MinecraftForge.EVENT_BUS.post(preMotionUpdateEvent))
+             return;
+        
         if (flag != this.serverSprintState)
         {
             if (flag)
