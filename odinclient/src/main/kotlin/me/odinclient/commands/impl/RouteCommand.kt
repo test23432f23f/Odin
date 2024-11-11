1947a807c2430697edaa50d3ea2c4d686f2d6805
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-11 11:42:38 +0100

    Update RouteCommand.kt

--- odinclient/src/main/kotlin/me/odinclient/commands/impl/RouteCommand.kt
+++ odinclient/src/main/kotlin/me/odinclient/commands/impl/RouteCommand.kt
@@ -29,13 +29,11 @@ val RouteCommand = commodore("route") {
 
                 updated.put(route.id, updatedList) 
                 RoutesManager.instance.loadedRoutes.put(AutoRouteUtils.currentRoomName, updated)
-                RoutesManager.instance.saveConfig("./config/routes.abc")
                 mc.thePlayer.addChatMessage(ChatComponentText("Added " + route.roomId + ", " + route.id + ", " + route.subId))
         
     }
     literal("remove").runs { id: Int, subId: Int ->
         RoutesManager.instance.loadedRoutes.getOrDefault(AutoRouteUtils.currentRoomName, HashMap()).getOrDefault(id, ArrayList()).removeAt(subId)
-        RoutesManager.instance.saveConfig("./config/routes.abc")
         mc.thePlayer.addChatMessage(ChatComponentText("Removed " + id + " " + subId))
     }
 
@@ -43,6 +41,16 @@ val RouteCommand = commodore("route") {
         AutoRouteUtils.currentRoom = null
         AutoRouteUtils.currentRoomName = "Unknown"
         mc.thePlayer.addChatMessage(ChatComponentText("Reset current room"))
+    }
+
+    literal("load").runs {
+        RoutesManager.instance.loadConfig("./config/routes.abc")
+        mc.thePlayer.addChatMessage(ChatComponentText("Loaded routes config"))
+    }
+
+    literal("save").runs {
+        RoutesManager.instance.saveConfig("./config/routes.abc")
+        mc.thePlayer.addChatMessage(ChatComponentText("Saved routes config"))
     }
 
     literal("rl").runs {
