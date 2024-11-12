37833efb059266fe196d2d9a114430e6a11deac1
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-12 04:16:00 +0100

    Update AutoRouteUtils.kt

--- odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
+++ odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
@@ -158,7 +158,7 @@ class AutoRouteUtils : Module(
                             mc.thePlayer.posY,
                             mc.thePlayer.posZ
                         ).distanceTo(if(currentRoom == null) route.pos else currentRoom!!.getRealCoords(route.pos))
-                                <= tolerance) && i < routes.size && i + 1 < routes.size && ((getSkyBlockID(mc.thePlayer.heldItem)
+                                <= tolerance) && i < routes.size && /*i + 1 < routes.size &&*/ ((getSkyBlockID(mc.thePlayer.heldItem)
                                 == "ASPECT_OF_THE_VOID") || getDisplayName(mc.thePlayer.heldItem).lowercase()
                             .contains("aspect of the void"))
                     ) {
@@ -167,8 +167,9 @@ class AutoRouteUtils : Module(
                         {
                             return
                         }
-                        val nextRoute = routes[i + 1]
-                        val (_, yaw, pitch) = etherwarpRotateTo(nextRoute.pos.toBlockPos())
+                        //val nextRoute = routes[i + 1]
+                        val: Float = route.yaw
+                        val: Float = route.pitch
                        
 
                         if(silentRotations)
