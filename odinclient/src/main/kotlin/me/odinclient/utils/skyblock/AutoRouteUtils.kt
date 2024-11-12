47160677b4ea398d3f88bb9861775905dc1a4e92
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-12 03:43:40 +0100

    Update AutoRouteUtils.kt

--- odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
+++ odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
@@ -168,8 +168,8 @@ class AutoRouteUtils : Module(
                             return
                         }
                         //val nextRoute = routes[i + 1]
-                        var yaw: Float = etherwarpRotateTo(BlockPos(route.pos)).getMiddle()
-                        var pitch: Float = etherwarpRotateTo(BlockPos(route.pos)).getRight()
+                        var yaw: Float = etherwarpRotateTo(BlockPos(route.pos)).yaw
+                        var pitch: Float = etherwarpRotateTo(BlockPos(route.pos)).pitch
 
                         if(silentRotations)
                         {
