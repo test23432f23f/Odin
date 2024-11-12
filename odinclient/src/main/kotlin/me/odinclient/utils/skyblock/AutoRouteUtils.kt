ae94cd25e6115e47f757e650834fc25db647993b
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-12 10:33:25 +0100

    Update AutoRouteUtils.kt

--- odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
+++ odinclient/src/main/kotlin/me/odinclient/utils/skyblock/AutoRouteUtils.kt
@@ -169,7 +169,7 @@ class AutoRouteUtils : Module(
                         }
                         //val nextRoute = routes[i + 1]
                         val yaw: Float = route.yaw + if(currentRoom != null) getRotation_(currentRoom!!.rotation) else 0
-                        val yaw: Float = route.pitch
+                        val pitch: Float = route.pitch
                        
 
                         if(silentRotations)
