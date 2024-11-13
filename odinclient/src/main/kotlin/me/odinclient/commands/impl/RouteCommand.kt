4352c98024f44474231b5b4e56ec8044fe559601
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-13 02:28:17 +0100

    Update RouteCommand.kt

--- odinclient/src/main/kotlin/me/odinclient/commands/impl/RouteCommand.kt
+++ odinclient/src/main/kotlin/me/odinclient/commands/impl/RouteCommand.kt
@@ -11,17 +11,46 @@ import net.minecraft.util.Vec3
 import net.minecraft.util.ChatComponentText
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils.getRelativeCoords
 import me.odinmain.utils.skyblock.dungeon.tiles.Rotations
+import me.odinmain.utils.skyblock.EtherWarpHelper
+import net.minecraft.util.MathHelper
+import net.minecraft.util.MovingObjectPosition
 
 
+fun method0(var0: Float, var1: Float, var2: Float): MovingObjectPosition {
+    val var3 = mc.thePlayer.getPositionEyes(1.0F)
+    val var4 = tr(var0, var1)
+    val var5 = var3.addVector(var4.xCoord * var2.toDouble(), var4.yCoord * var2.toDouble(), var4.zCoord * var2.toDouble())
+    return mc.theWorld.rayTraceBlocks(var3, var5, true, true, true)
+}
+
+fun tr(var0: Float, var1: Float): Vec3 {
+    val var2 = MathHelper.cos(-var0 * 0.017453292F - 3.1415927F)
+    val var3 = MathHelper.sin(-var0 * 0.017453292F - 3.1415927F)
+    val var4 = -MathHelper.cos(-var1 * 0.017453292F)
+    val var5 = MathHelper.sin(-var1 * 0.017453292F)
+    return Vec3(var3 * var4.toDouble(), var5.toDouble(), var2 * var4.toDouble())
+}
 
 val RouteCommand = commodore("route") {
+
+
+    
     literal("add").runs { subId: Int, type: String ->
+
+                val mop: MovingObjectPosition = method0(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch, 61.0F)
+                if(mop.typeOfHit !=  MovingObjectPosition.BLOCK)
+                {
+                    mc.thePlayer.addChatMessage(ChatComponentText("UNKNOWN POSITION"))
+
+                    return
+                }
+        
                 val route = RoutesManager.Route(
                     RoutesManager.Route.RouteType.valueOf(type),
                     AutoRouteUtils.currentRoomName,
                     subId.toInt(),
                     RoutesManager.instance.loadedRoutes.getOrDefault(AutoRouteUtils.currentRoomName, HashMap()).getOrDefault(subId.toInt(), ArrayList()).size,
-                    if(AutoRouteUtils.currentRoom != null) AutoRouteUtils.currentRoom!!.getRelativeCoords(Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ)) else Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ),
+                    if(AutoRouteUtils.currentRoom != null) AutoRouteUtils.currentRoom!!.getRelativeCoords(mop.hitVec) else Vec3(mop.hitVec),
                     MathHelper.wrapAngleTo180_float(mc.thePlayer.rotationYaw),
                     mc.thePlayer.rotationPitch
                 )
@@ -34,20 +63,30 @@ val RouteCommand = commodore("route") {
                 RoutesManager.instance.loadedRoutes.put(AutoRouteUtils.currentRoomName, updated)
                 RoutesManager.instance.saveConfig("./config/routes.abc")
                 mc.thePlayer.addChatMessage(ChatComponentText("Added " + route.roomId + ", " + route.id + ", " + route.subId))
+
+                EtherWarpHelper
         
     }
 
     literal("set").runs { id: Int, subId: Int ->
         val updated = RoutesManager.instance.loadedRoutes.getOrDefault(AutoRouteUtils.currentRoomName, HashMap())
         val updatedList: MutableList<RoutesManager.Route> = updated.getOrDefault(id.toInt(), ArrayList())
+
+         val mop: MovingObjectPosition = method0(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch, 61.0F)
+                if(mop.typeOfHit !=  MovingObjectPosition.BLOCK)
+                {
+                    mc.thePlayer.addChatMessage(ChatComponentText("UNKNOWN POSITION"))
+
+                    return
+                }
         
         val route = RoutesManager.Route(
                     updatedList.get(subId.toInt()).type,
                     AutoRouteUtils.currentRoomName,
                     subId.toInt(),
                     id.toInt(),
-                    if(AutoRouteUtils.currentRoom != null) AutoRouteUtils.currentRoom!!.getRelativeCoords(Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ)) else Vec3(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ),
-                   MathHelper.wrapAngleTo180_float(mc.thePlayer.rotationYaw),
+                    if(AutoRouteUtils.currentRoom != null) AutoRouteUtils.currentRoom!!.getRelativeCoords(mop.hitVec) else Vec3(mop.hitVec),
+                       MathHelper.wrapAngleTo180_float(mc.thePlayer.rotationYaw),
                     mc.thePlayer.rotationPitch
                 )
 
