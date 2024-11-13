13f03e40388887c686cdaab5841813317db3d72c
test23432f23f <124468613+test23432f23f@users.noreply.github.com>
2024-11-13 23:56:32 +0100

    Update MixinEntityPlayerSP.java

--- odinclient/src/main/java/me/odinclient/mixin/mixins/entity/MixinEntityPlayerSP.java
+++ odinclient/src/main/java/me/odinclient/mixin/mixins/entity/MixinEntityPlayerSP.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import me.odinmain.events.impl.MotionUpdateEvent;
+import me.odinmain.events.impl.MotionUpdateEventPost;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Final;
